e5f219d417a9bf0f403041e811a5d96d7a545a00
MarcioLeoS <167657111+MarcioLeoS@users.noreply.github.com>
2025-11-26 16:15:07 -0300

    fix

--- infrastructure/mysql/seed/00-frink-db-and-user.sh
+++ infrastructure/mysql/seed/00-frink-db-and-user.sh
@@ -3,9 +3,10 @@ set -e
 
 echo "Inicializando base de datos Frink Assistant..."
 
-mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS ${FRINK_DB} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${FRINK_DB}\`;"
+mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "ALTER DATABASE \`${FRINK_DB}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
 mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS '${FRINK_DB_USER}'@'%' IDENTIFIED BY '${FRINK_DB_PASSWORD}';"
-mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON ${FRINK_DB}.* TO '${FRINK_DB_USER}'@'%';"
+mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON \`${FRINK_DB}\`.* TO '${FRINK_DB_USER}'@'%';"
 mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT SHOW DATABASES ON *.* TO '${FRINK_DB_USER}'@'%';"
 mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
 
